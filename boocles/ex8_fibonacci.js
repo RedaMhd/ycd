48c63106a930ec7d7f58abb2166f4e66ef6c9e7e
RedaMhd <mouhdi.reda1@gmail.com>
2026-09-06 12:54:58 +0100

    pass to prj

--- boocles/ex8_fibonacci.js
+++ boocles/ex8_fibonacci.js
@@ -1,18 +1,25 @@
 const pr = require("prompt-sync")();
 
 let nbr = parseInt(pr('enter a number : '));
+
 let i = 2;
 let result;
 let res1 = 1;
 let _0 = 0;
-if(nbr == 0 || nbr == 1)
-{ 
+let h;
+
+if (nbr == 0) {
+    console.log(0);
+} else if (nbr == 1) {
     console.log(1);
+} else {
+    while (i <= nbr) {
+        h = res1;
+        result = res1 + _0;
+        _0 = h;
+        res1 = result;
+        i++;
+    }
+
+    console.log(result);
 }
-while(i < nbr){
-    result = res1 + _0;
-    res1 += _0;
-    _0++;
-    i++;
-}
-console.log(result);
